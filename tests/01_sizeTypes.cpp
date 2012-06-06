b95f6fa44267ecbd540fdffad57c10509db6fc80
Lambert Clara <lambert.clara@yahoo.fr>
2012-06-06 21:52:31 +0000

    Use C++11 static assertions when testing types sizes

--- tests/01_sizeTypes.cpp
+++ tests/01_sizeTypes.cpp
@@ -1,42 +1,35 @@
 #include <iostream>
-#include <cassert>
 #include "SE_Types.h"
 
 int main()
 {
-    F32 float32 = 0.f;
-    std::cout << "F32 float32 size : " << sizeof(float32) * 8 << std::endl;
-    assert(sizeof(float32) == 4);
+    static_assert(sizeof(F32) == 4,
+                  "sizeof(float32) != 4 on this machine");
 
-    U8 uint8 = 0;
-    std::cout << "U8 uint8 size : " << sizeof(uint8) * 8 << std::endl;
-    assert(sizeof(uint8) == 1);
+    static_assert(sizeof(U8) == 1,
+                  "sizeof(U8) != 1 on this machine");
 
-    I8 int8 = 0;
-    std::cout << "I8 int8 size : " << sizeof(int8) * 8 << std::endl;
-    assert(sizeof(int8) == 1);
+    static_assert(sizeof(I8) == 1,
+                  "sizeof(I8) != 1 on this machine");
 
-    U16 uint16 = 0;
-    std::cout << "U16 uint16 size : " << sizeof(uint16) * 8 << std::endl;
-    assert(sizeof(uint16) == 2);
+    static_assert(sizeof(U16) == 2,
+                  "sizeof(U16) != 2 on this machine");
 
-    I16 int16 = 0;
-    std::cout << "I16 int16 size : " << sizeof(int16) * 8 << std::endl;
-    assert(sizeof(int16) == 2);
+    static_assert(sizeof(I16) == 2,
+                  "sizeof(I16) != 2 on this machine");
 
-    U32 uint32 = 0;
-    std::cout << "U32 uint32 size : " << sizeof(uint32) * 8 << std::endl;
-    assert(sizeof(uint32) == 4);
+    static_assert(sizeof(U32) == 4,
+                  "sizeof(U32) != 4 on this machine");
 
-    I32 int32 = 0;
-    std::cout << "I32 int32 size : " << sizeof(int32) * 8 << std::endl;
-    assert(sizeof(int32) == 4);
+    static_assert(sizeof(I32) == 4,
+                  "sizeof(I32) != 4 on this machine");
 
-    U64 uint64 = 0;
-    std::cout << "U64 uint64 size : " << sizeof(uint64) * 8 << std::endl;
-    assert(sizeof(uint64) == 8);
+    static_assert(sizeof(U64) == 8,
+                  "sizeof(U64) != 8 on this machine");
 
-    I64 int64 = 0;
-    std::cout << "I64 int64 size : " << sizeof(int64) * 8 << std::endl;
-    assert(sizeof(int64) == 8);
+    static_assert(sizeof(I64) == 8,
+                  "sizeof(I64) != 8 on this machine");
+
+    return EXIT_SUCCESS;
 }
+
